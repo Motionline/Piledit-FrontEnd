2b8170d3da936123b1d79b7a41a9c972408f618b
Momeemt <momiimt@gmail.com>
2020-08-23 13:13:11 +0900

    ✨ Add: SVGTextの追加
    SVG内で扱うテキストをコンポーネントにしたもの

---
+++ src/components/Atoms/SVGText.vue
@@ -0,0 +1,35 @@
+<template>
+  <text
+    :x="x"
+    :y="y"
+    :fill="color"
+    font-size="14"
+    class="notFocusSVGText"
+  >
+    <slot />
+  </text>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+@Component
+export default class SVGText extends Vue {
+  @Prop({ default: '0' })
+  public x!: string
+
+  @Prop({ default: '0' })
+  public y!: string
+
+  @Prop()
+  public color!: string
+}
+</script>
+
+<style lang="scss" scoped>
+  .notFocusSVGText {
+    -ms-user-select: none;
+    -webkit-user-select: none;
+    user-select: none;
+    pointer-events: none;
+  }
+</style>
