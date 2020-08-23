e441a479545a13a3752c1fab54bfb30292812ce9
Momeemt <momiimt@gmail.com>
2020-08-23 18:09:27 +0900

    ✨ Add: EventBlockBaseの追加
    EventBlockのベースコンポーネント

---
+++ src/components/Molecules/EventBlockBase.vue
@@ -0,0 +1,45 @@
+<template>
+  <svg
+    @mousedown.left="mouseDown($event)"
+    @click.right.prevent="popupContextMenu($event)"
+    preserveAspectRatio="xMidYMid meet"
+    :x="x"
+    :y="y"
+    :id="blockUniqueKey"
+  >
+    <path
+      :d="path"
+      :stroke="strokeColor"
+      :fill="fillColor"
+      stroke-width="2"
+      transform="translate(1,15) scale(0.75, 0.75)"
+    />
+    <slot></slot>
+    <path
+      v-if="showShadow"
+      stroke-width="2"
+      fill="#d3d3d8"
+      :d="shadowPath"
+      transform="translate(1,52) scale(0.75, 0.75)"
+    />
+  </svg>
+</template>
+
+<script lang="ts">
+import { Prop, Emit, Vue } from 'vue-property-decorator'
+import svgZOrder from 'svgZOrder'
+const remote = require('electron').remote
+const Menu = remote.Menu
+const MenuItem = remote.MenuItem
+export default class SVGText extends Vue {
+  @Prop()
+  public blockUniqueKey!: string
+
+  @Prop()
+  public x!: number
+}
+</script>
+
+<style scoped>
+
+</style>
