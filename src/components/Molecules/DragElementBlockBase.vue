e35a7361da0789db11da9e26de091d6570cc3ac1
Momeemt <momiimt@gmail.com>
2020-10-24 09:37:18 +0900

    👍  ReImpl: DragElementBlockBase.vueをTypescriptに書き換え

--- src/components/Molecules/DragElementBlockBase.vue
+++ src/components/Molecules/DragElementBlockBase.vue
@@ -11,29 +11,19 @@
   </svg>
 </template>
 
-<script>
-export default {
-  name: 'DragElementBlockBase',
-  props: {
-    // pathの輪郭・塗り潰し色
-    strokeColor: {
-      type: String,
-      required: true
-    },
-    fillColor: {
-      type: String,
-      required: true
-    },
-    // ブロックの横の長さ
-    width: {
-      type: String,
-      required: true
-    }
-  },
-  data () {
-    return {
-      path: `m 0,4 A 4,4 0 0,1 4,0 H 12 c 2,0 3,1 4,2 l 4,4 c 1,1 2,2 4,2 h 12 c 2,0 3,-1 4,-2 l 4,-4 c 1,-1 2,-2 4,-2 H ${this.width} a 4,4 0 0,1 4,4 v 40  a 4,4 0 0,1 -4,4 H 48   c -2,0 -3,1 -4,2 l -4,4 c -1,1 -2,2 -4,2 h -12 c -2,0 -3,-1 -4,-2 l -4,-4 c -1,-1 -2,-2 -4,-2 H 4 a 4,4 0 0,1 -4,-4 z`
-    }
-  }
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+@Component
+export default class DragElementBlockBase extends Vue {
+  @Prop({ required: true })
+  public strokeColor!: string
+
+  @Prop({ required: true })
+  public fillColor!: string
+
+  @Prop({ required: true })
+  public width!: number
+
+  public path = `m 0,4 A 4,4 0 0,1 4,0 H 12 c 2,0 3,1 4,2 l 4,4 c 1,1 2,2 4,2 h 12 c 2,0 3,-1 4,-2 l 4,-4 c 1,-1 2,-2 4,-2 H ${this.width} a 4,4 0 0,1 4,4 v 40  a 4,4 0 0,1 -4,4 H 48   c -2,0 -3,1 -4,2 l -4,4 c -1,1 -2,2 -4,2 h -12 c -2,0 -3,-1 -4,-2 l -4,-4 c -1,-1 -2,-2 -4,-2 H 4 a 4,4 0 0,1 -4,-4 z`
 }
 </script>
