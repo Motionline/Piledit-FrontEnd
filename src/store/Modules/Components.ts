e1497ed04b7061b3b2e81abe576540e0adcea8a3
Momeemt <momiimt@gmail.com>
2020-10-24 14:18:33 +0900

    👍  ReImpl: Components StoreをTSに書き換えた

---
+++ src/store/Modules/Components.ts
@@ -0,0 +1,54 @@
+import {
+  Module,
+  VuexModule,
+  Mutation,
+  Action,
+  getModule,
+} from 'vuex-module-decorators'
+import { Vue } from 'vue-property-decorator'
+import store from '@/store/store'
+
+type Component = {
+  componentUniqueKey: string
+}
+
+export interface IComponentsState {
+  allComponents: { [key: string]: Component }
+}
+
+@Module({ dynamic: true, store: store, name: 'Components', namespaced: true })
+class Components extends VuexModule implements IComponentsState {
+  allComponents: { [key: string]: Component } = {}
+
+  @Mutation
+  public addComponent (component: Component) {
+    Vue.set(this.allComponents, component.componentUniqueKey, component)
+  }
+
+  @Mutation
+  public removeComponent (componentUniqueKey: string) {
+    Vue.delete(this.allComponents, componentUniqueKey)
+  }
+
+  @Mutation
+  public updateComponent (component: Component) {
+    Vue.set(this.allComponents, component.componentUniqueKey, component)
+  }
+
+  @Action
+  public add (component: Component) {
+    this.addComponent(component)
+  }
+
+  @Action
+  public remove (componentUniqueKey: string) {
+    this.removeComponent(componentUniqueKey)
+  }
+
+  @Action
+  public update (component: Component) {
+    this.updateComponent(component)
+  }
+}
+
+export const componentsModule = getModule(Components)
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import { IBlockState } from '@/store/Modules/Blocks'
+import { IComponentsState } from '@/store/Modules/Components'
+
 Vue.use(Vuex)
 
 export interface State {
-  blocks: IBlockState
+  blocks: IBlockState,
+  components: IComponentsState
 }
 export default new Vuex.Store<State>({})
