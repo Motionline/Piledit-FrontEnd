f7ff3ade9d2296e56f9f28c52d3b92b0e4baf201
Momeemt <momiimt@gmail.com>
2020-10-24 15:51:18 +0900

    👍  ReImpl: Timeline StoreをTypeScriptに書き換え

---
+++ src/store/Modules/Timeline.ts
@@ -0,0 +1,84 @@
+import {
+  Module,
+  VuexModule,
+  Mutation,
+  Action,
+  getModule,
+} from 'vuex-module-decorators'
+import { Vue } from 'vue-property-decorator'
+import store from '@/store/store'
+
+type Position = {
+  x: number
+  y: number
+}
+
+type ComponentObject = {
+  componentObjectUniqueKey: string
+  componentUniqueKey: string
+  position: Position
+  width: number
+}
+
+export interface ITimeLineState {
+  componentObjects: { [key: string]: ComponentObject }
+}
+
+@Module({ dynamic: true, store: store, name: 'Components', namespaced: true })
+class TimeLine extends VuexModule implements ITimeLineState {
+  componentObjects: { [key: string]: ComponentObject } = {}
+
+  @Mutation
+  public addComponentObject (componentObject: ComponentObject) {
+    Vue.set(this.componentObjects, componentObject.componentObjectUniqueKey, componentObject)
+  }
+
+  @Mutation
+  public updateComponentObjectPosition (componentObject: ComponentObject) {
+    this.componentObjects[componentObject.componentObjectUniqueKey].position = componentObject.position
+  }
+
+  @Mutation
+  public updateComponentObjectWidth (componentObject: ComponentObject) {
+    this.componentObjects[componentObject.componentObjectUniqueKey].width = componentObject.width
+  }
+
+  @Action({})
+  public add (componentUniqueKey: string) {
+    const componentObjectUniqueKey = generateUuid()
+    const componentObject: ComponentObject = {
+      componentObjectUniqueKey,
+      componentUniqueKey: componentUniqueKey,
+      position: {
+        x: 0,
+        y: 1
+      },
+      width: 200
+    }
+    this.addComponentObject(componentObject)
+  }
+
+  @Action({})
+  public updatePosition (componentObject: ComponentObject) {
+    this.updateComponentObjectPosition(componentObject)
+  }
+
+  @Action({})
+  public updateWidth (componentObject: ComponentObject) {
+    this.updateComponentObjectWidth(componentObject)
+  }
+}
+
+const generateUuid = () => {
+  const material = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.split('')
+  for (let i = 0, len = material.length; i < len; i++) {
+    if (material[i] === 'x') {
+      material[i] = Math.floor(Math.random() * 16).toString(16)
+    } else if (material[i] === 'y') {
+      material[i] = (Math.floor(Math.random() * 4) + 8).toString(16)
+    }
+  }
+  return material.join('')
+}
+
+export const timelineModule = getModule(TimeLine)
--- src/store/store.ts
+++ src/store/store.ts
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import { IBlockState } from '@/store/Modules/Blocks'
 import { IComponentsState } from '@/store/Modules/Components'
+import { ITimeLineState } from '@/store/Modules/Timeline'
 
 Vue.use(Vuex)
 
 export interface State {
   blocks: IBlockState,
-  components: IComponentsState
+  components: IComponentsState,
+  timeline: ITimeLineState
 }
 export default new Vuex.Store<State>({})
