f48716eace0d40f92eb862613f56ce01db5e94f8
Momeemt <momiimt@gmail.com>
2021-01-10 21:13:41 +0900

    👍  Add: routeの追加

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,29 +1,18 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '@/views/Home.vue'
+import TimeLine from '@/views/TimeLine.vue'
 import ComponentsEdit from '@/views/ComponentsEdit.vue'
 
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path: '/components_edit/:uuid',
-    name: 'ComponentsEdit',
-    component: ComponentsEdit
-  }
+  { path: '/', name: 'Home', component: () => import('../views/Home.vue') },
+  { path: '/preferences' }, // アプリの設定
+  { path: '/users/:userId' }, // ユーザー表示
+  { path: '/users/settings' }, // ユーザー編集
+  { path: '/newProject', name: 'NewProject', component: () => import('../views/NewProject.vue') },
+  { path: '/timeline/:projectUuid', name: 'TimeLine', component: () => import('../views/TimeLine.vue') },
+  { path: '/components_edit/:projectUuid/:tabUuid', name: 'ComponentsEdit', component: () => import('../views/ComponentsEdit.vue') }
 ]
 
 const router = new VueRouter({
