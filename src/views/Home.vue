d52933b4cb914c97d5d7374acab2e1daaa4326cf
Momeemt <momiimt@gmail.com>
2021-01-10 21:11:49 +0900

    👍  Add: プロジェクト作成画面の作成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="Home">
     <h1>Pileditへようこそ</h1>
-    <v-btn>新規プロジェクト</v-btn>
+    <v-btn to="/newProject">新規プロジェクト</v-btn>
     <v-btn>プロジェクトを開く</v-btn>
     <p>アカウント設定</p>
     <v-btn>アカウント作成</v-btn>
@@ -10,8 +10,15 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
+import { BrowserWindow, remote } from 'electron'
 
-@Component
+@Component({
+  head: {
+    script: [
+      { type: 'text/javascript', src: '@/assets/adobefont.js', async: true }
+    ]
+  }
+})
 export default class Home extends Vue {
 }
 </script>
@@ -21,5 +28,10 @@ export default class Home extends Vue {
   background-color: #203744;
   color: white;
   height: 100vh;
+
+  h1 {
+    font-family: a-otf-futo-go-b101-pr6n, sans-serif;
+    font-weight: bold;
+  }
 }
 </style>
--- src/views/NewProject.vue
+++ src/views/NewProject.vue
@@ -1,10 +1,20 @@
 <template>
-$END$
+  <div>
+    <p>新しくプロジェクトを作成する</p>
+  </div>
 </template>
 
-<script>
-export default {
-name: "NewProject.vue"
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({
+  head: {
+    script: [
+      { type: 'text/javascript', src: '@/assets/adobefont.js', async: true }
+    ]
+  }
+})
+export default class NewProject extends Vue {
 }
 </script>
 
