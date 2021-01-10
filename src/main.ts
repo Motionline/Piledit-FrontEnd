89324e65408b9a28dd4cab8bf1e012f2650a7ac0
Momeemt <momiimt@gmail.com>
2021-01-10 21:14:07 +0900

    👍  Add: VueHeadを読み込んだ

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store/store'
 import vuetify from './plugins/vuetify'
+import VueHead from 'vue-head'
 // import axios from 'axios'
 // import VueAxios from 'vue-axios'
 
 Vue.config.productionTip = false
 // Vue.use(axios)
+
+Vue.use(VueHead)
 
 new Vue({
   router,
