38370ac75a4f921c588940d9cb7084a10000ade8
Momeemt <momiimt@gmail.com>
2021-01-11 19:05:16 +0900

    👍  Change: タブごとにリンクを分けた
    異なるリンクじゃないとtabsのスライダーが動かない

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,13 +4,13 @@ import VueRouter, { RouteConfig } from 'vue-router'
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
-  { path: '/', name: 'Home', component: () => import('../views/Home.vue') },
   { path: '/preferences' }, // アプリの設定
   { path: '/users/:userId' }, // ユーザー表示
   { path: '/users/settings' }, // ユーザー編集
-  { path: '/projects/new', name: 'NewProject', component: () => import('../views/NewProject.vue') },
+  { path: '/:tabUuid/projects/new', name: 'NewProject', component: () => import('../views/NewProject.vue') },
   { path: '/projects/:project_uuid/components_edit/:tabUuid', name: 'ComponentsEdit', component: () => import('../views/ComponentsEdit.vue') },
-  { path: '/projects/:project_uuid', name: 'TimeLine', component: () => import('../views/TimeLine.vue') }
+  { path: '/:tabUuid/projects/:project_uuid', name: 'TimeLine', component: () => import('../views/TimeLine.vue') },
+  { path: '/:tabUuid', name: 'Home', component: () => import('../views/Home.vue') }
 ]
 
 const router = new VueRouter({
