74571adfe5fd83645e0d262b24858a509ed416d3
Momeemt <momiimt@gmail.com>
2021-01-15 22:33:33 +0900

    🎨  Design: Welcome画面をデザイン

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,35 @@
 <template>
   <div id="Home">
-    <h1>Pileditへようこそ</h1>
-    <v-btn @click="toNewProject">新規プロジェクト</v-btn>
-    <v-btn>プロジェクトを開く</v-btn>
-    <p>アカウント設定</p>
-    <v-btn>アカウント作成</v-btn>
+    <div class="Home__wrapper">
+      <img src="../assets/piledit.png" alt="piledit-logo"  width="395px" />
+      <h2>Simple, Componentable, Expandable</h2>
+      <div class="Home__wrapper--links">
+        <div class="Home__wrapper--links--group">
+          <h3>スタート</h3>
+          <div>
+            <a @click="toNewProject">新規プロジェクト</a>
+          </div>
+          <div>
+            <a>プロジェクトを開く</a>
+          </div>
+        </div>
+        <div class="Home__wrapper--links--group">
+          <h3>直近のプロジェクト</h3>
+        </div>
+        <div class="Home__wrapper--links--group">
+          <h3>アカウント設定</h3>
+          <div>
+            <a>アカウントを作成する</a>
+          </div>
+        </div>
+        <div class="Home__wrapper--links--group">
+          <h3>Pileditについて学ぶ</h3>
+        </div>
+        <div class="Home__wrapper--links--group">
+          <h3>ヘルプ</h3>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -13,14 +38,7 @@ import { Component, Vue } from 'vue-property-decorator'
 import { tabsModule } from '@/store/store'
 import { PTabHistoryKind } from '@/@types/piledit'
 
-@Component({
-// ずっとエラー起こるから解決したら使う
-//   head: {
-//     script: [
-//       { type: 'text/javascript', src: '@/assets/adobefont.js', async: true }
-//     ]
-//   }
-})
+@Component({})
 export default class Home extends Vue {
   toNewProject () {
     const uuid = tabsModule.currentViewingTabUuid
@@ -33,13 +51,35 @@ export default class Home extends Vue {
 
 <style scoped lang="scss">
 #Home {
-  background-color: #203744;
-  color: white;
-  height: 100vh;
+  // background-color: #203744;
+  color: #898989;
+  height: calc(100vh - 96px);
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-  h1 {
-    font-family: a-otf-futo-go-b101-pr6n, sans-serif;
+  .Home__wrapper--links {
+    margin-top: 40px;
+
+    &--group {
+      margin-top: 20px;
+    }
+  }
+
+  h2, h3, p {
+    font-family: tbchibirgothicplusk-pro, sans-serif;
+
     font-weight: bold;
+
+    font-style: normal;
+  }
+
+  a {
+    font-family: tbchibirgothicplusk-pro, sans-serif;
+
+    font-weight: normal;
+
+    font-style: normal;
   }
 }
 </style>
