4dfc5b655359f44ef5be6a8ff123a952bfa13fb8
Momeemt <momiimt@gmail.com>
2021-01-19 17:09:54 +0900

    ✨  Add: Piledit-Storeを作成

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,6 +7,7 @@ const routes: Array<RouteConfig> = [
   { path: '/preferences' }, // アプリの設定
   { path: '/users/:userId' }, // ユーザー表示
   { path: '/users/settings' }, // ユーザー編集
+  { path: '/:tabUuid/store', name: 'PileditStore', component: () => import('../views/PileditStore.vue') },
   { path: '/:tabUuid/projects/new', name: 'NewProject', component: () => import('../views/NewProject.vue') },
   { path: '/:tabUuid/projects/:projectUuid/components/:componentUuid', name: 'ComponentsEdit', component: () => import('../views/ComponentsEdit.vue') },
   { path: '/:tabUuid/projects/:projectUuid', name: 'TimeLine', component: () => import('../views/TimeLine.vue') },
---
+++ src/views/PileditStore.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="piledit-store">
+    <h1>ストア</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({})
+export default class PileditStore extends Vue {
+
+}
+</script>
+
+<style scoped>
+#piledit-store {
+  font-family: tbchibirgothicplusk-pro, sans-serif;
+  font-weight: bold;
+  font-style: normal;
+}
+</style>
