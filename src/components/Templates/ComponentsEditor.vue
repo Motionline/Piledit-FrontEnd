adeb21e9138d48de96b9327a1f06d29f1cfa8947
Momeemt <momiimt@gmail.com>
2021-01-21 17:29:39 +0900

    👍  Add: プレビュー再生画面の実装

--- src/components/Templates/ComponentsEditor.vue
+++ src/components/Templates/ComponentsEditor.vue
@@ -6,13 +6,37 @@
         :tab-uuid="tabUuid"
         :project-uuid="projectUuid"
         @openingMenu="openingMenu"
+        class="componentsEditor__container--sandbox"
     />
-    <BlockDetailedPanel
-        :block="getBlock(blockUuid)"
-        :component-uuid="componentUuid"
-        :tab-uuid="tabUuid"
-        :project-uuid="projectUuid"
-    />
+    <div class="componentsEditor__container--detail">
+      <div class="componentsEditor__container--detail--preview">
+        <video-player  class="video-player-box"
+                       ref="videoPlayer"
+                       :options="playerOptions"
+                       :playsinline="true"
+                       customEventName="customstatechangedeventname"
+
+                       @play="onPlayerPlay($event)"
+                       @pause="onPlayerPause($event)"
+                       @ended="onPlayerEnded($event)"
+                       @waiting="onPlayerWaiting($event)"
+                       @playing="onPlayerPlaying($event)"
+                       @loadeddata="onPlayerLoadeddata($event)"
+                       @timeupdate="onPlayerTimeupdate($event)"
+                       @canplay="onPlayerCanplay($event)"
+                       @canplaythrough="onPlayerCanplaythrough($event)"
+
+                       @statechanged="playerStateChanged($event)"
+                       @ready="playerReadied">
+        </video-player>
+      </div>
+      <BlockDetailedPanel
+          :block="getBlock(blockUuid)"
+          :component-uuid="componentUuid"
+          :tab-uuid="tabUuid"
+          :project-uuid="projectUuid"
+      />
+    </div>
   </div>
 </template>
 
@@ -41,6 +65,19 @@ export default class ComponentsEditor extends Vue {
   public projectUuid!: string
 
   public blockUuid = ''
+  public playerOptions = {
+    // videojs options
+    muted: true,
+    language: 'ja',
+    // height: '230',
+    width: window.parent.screen.width * 0.30,
+    playbackRates: [0.7, 1.0, 1.5, 2.0],
+    sources: [{
+      type: 'video/mp4',
+      src: 'https://cdn.theguardian.tv/webM/2015/07/20/150716YesMen_synd_768k_vp8.webm'
+    }],
+    poster: '/static/images/author.jpg'
+  }
 
   get blocks () {
     return blocksModule.blocks
@@ -67,11 +104,51 @@ export default class ComponentsEditor extends Vue {
   public openingMenu (uuid: string) {
     this.blockUuid = uuid
   }
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  public onPlayerPlay (event: any) {}
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  public onPlayerPause (event: any) {}
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  public onPlayerEnded (event: any) {}
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  public onPlayerWaiting (event: any) {}
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  public onPlayerPlaying (event: any) {}
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  public onPlayerLoadeddata (event: any) {}
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  public onPlayerTimeupdate (event: any) {}
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  public onPlayerCanplay (event: any) {}
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  public onPlayerCanplaythrough (event: any) {}
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  public playerStateChanged (event: any) {}
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  public playerReadied () {}
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 .componentsEditor__container {
   height: calc(100vh - 96px);
+  display: flex;
+  width: 100vw;
+
+  &--sandbox {
+    width: 70vw;
+  }
+
+  &--detail {
+    width: 30vw;
+
+    &--preview {
+      height: 30%;
+    }
+  }
+}
+.vjs-tech:focus {
+  outline: none !important;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import store, { sessionsModule } from './store/store'
 import vuetify from './plugins/vuetify'
 import VueHead from 'vue-head'
 import adobeLoader from './assets/adobefont'
+import VueVideoPlayer from 'vue-video-player'
+import 'video.js/dist/video-js.css'
 import { firestorePlugin } from 'vuefire'
 import { Auth } from '@/firebase/auth'
 import { MenuMixin } from '@/mixin/menu'
@@ -19,6 +21,7 @@ Vue.config.productionTip = false
 
 Vue.use(VueHead)
 Vue.use(firestorePlugin)
+Vue.use(VueVideoPlayer)
 
 adobeLoader(document)
 
