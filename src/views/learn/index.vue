9e4ecb5b66033506e9cf7f9ce109313b1add27fd
Momeemt <momiimt@gmail.com>
2021-01-24 13:09:17 +0900

    ✏️  Add: チュートリアルを書き始めた

--- src/views/learn/index.vue
+++ src/views/learn/index.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="learnIndex">
     <h1>Piledit チュートリアル</h1>
+    <p>
+      Pileditの概念について説明していきます。
+    </p>
+    <h2>Component</h2>
+    <h2>Prop</h2>
+    <p>
+      コンポーネントはパラメータを受け取ることができます。これをPropと呼びます。
+    </p>
+    <h2>Magic Project</h2>
   </div>
 </template>
 
