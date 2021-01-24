5a6ea7f2459267bb534769b6db8583f27e384629
Momeemt <momiimt@gmail.com>
2021-01-24 12:16:22 +0900

    👍  Add: Commentを追加してFirestoreに登録する

--- src/store/Modules/Comments.ts
+++ src/store/Modules/Comments.ts
@@ -1,8 +1,10 @@
 import { Action, Module, Mutation, VuexModule } from 'vuex-module-decorators'
-import store from '@/store/store'
+import store, { usersModule } from '@/store/store'
 import firebase from 'firebase'
 import { DB } from '@/firebase/db'
-import { PComments } from '@/@types/piledit'
+import { PComment, PComments } from '@/@types/piledit'
+import { Vue } from 'vue-property-decorator'
+import { VuexMixin } from '@/mixin/vuex'
 
 export interface CommentStateIF {
   comments: PComments;
@@ -11,4 +13,26 @@ export interface CommentStateIF {
 @Module({ store: store, name: 'CommentsModule', namespaced: true })
 export default class Comments extends VuexModule implements CommentStateIF {
   comments: PComments = {}
+
+  @Mutation
+  public addComment ({ comment }: { comment: PComment }) {
+    Vue.set(this.comments, comment.uuid, comment)
+  }
+
+  @Action({ rawError: true })
+  public async add ({ content, topicUuid }: { content: string; topicUuid: string }) {
+    const uuid = VuexMixin.generateUuid()
+    const commentRef = DB.collection('comments').doc(uuid)
+    const comment = new PComment({
+      uuid,
+      content,
+      topicUuid,
+      userUuid: usersModule.email
+    })
+    await commentRef.set({
+      ...comment,
+      updateAt: firebase.firestore.FieldValue.serverTimestamp()
+    }, { merge: true })
+    this.addComment({ comment })
+  }
 }
